7f0685ce9bc92c927aa8bcfb8c7d0fee87601ca2
Rupa Gadewar <rupa@gadewar.com>
2023-10-08 21:05:14 -0500

    Netlify configuration

---
+++ client/build/precache-manifest.e35cff85e5ea652949bb89d55773ad88.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6709ae1d261dad277e07b53773040f53",
+    "url": "/index.html"
+  },
+  {
+    "revision": "977eac2051f047036cf3",
+    "url": "/static/css/main.a64bd9e7.chunk.css"
+  },
+  {
+    "revision": "922cc10d70168817f36a",
+    "url": "/static/js/2.bdc2a7bf.chunk.js"
+  },
+  {
+    "revision": "6865f50f3618af47c938d1e1cd5951cf",
+    "url": "/static/js/2.bdc2a7bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "977eac2051f047036cf3",
+    "url": "/static/js/main.63de0e9e.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.63de0e9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d52f44d6fb82598fed4",
+    "url": "/static/js/runtime-main.c62b8046.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
